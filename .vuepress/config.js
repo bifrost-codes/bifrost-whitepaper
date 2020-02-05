810781971b9015819c2918144429a49fce4971df
lurpis <hi@lurpis.com>
2020-02-06 02:09:12 +0800

    Update language

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
     lineNumbers: true,
   },
   locales: {
-    '/': {
+    '/en/': {
       lang: 'en-US',
       title: 'Bifrost Whitepaper',
       description: 'A Parachain Designed For Staking Liquidity',
@@ -19,7 +19,7 @@ module.exports = {
     displayAllHeaders: true,
     search: true,
     locales: {
-      '/': {
+      '/en/': {
         selectText: 'Languages',
         label: 'English',
         ariaLabel: 'Languages',
@@ -45,62 +45,77 @@ module.exports = {
           {text: 'GitHub', link: 'https://github.com/bifrost-codes'},
         ],
         sidebar: [
-          // {
-          //   title: 'Getting Started',
-          //   collapsable: false,
-          //   sidebarDepth: 0,
-          //   children: [
-          //     ['getting_started/01_marketing.md', 'Marketing'],
-          //     ['getting_started/02_overview.md', 'Overview'],
-          //   ],
-          // },
-          // {
-          //   title: 'Economic',
-          //   collapsable: false,
-          //   sidebarDepth: 0,
-          //   children: [
-          //     ['economic/01_bnc_economic.md', 'BNC Economic'],
-          //     ['economic/02_three_pools_valuation_model.md', 'Valuation Model'],
-          //     ['economic/03_staking_gain_mechanism.md', 'Staking Gain Mechanism'],
-          //     ['economic/04_stake_proxy_node.md', 'Stake Proxy Node'],
-          //   ],
-          // },
-          // {
-          //   title: 'System Structure',
-          //   collapsable: false,
-          //   sidebarDepth: 0,
-          //   children: [
-          //     ['system_structure/01_system_structure.md', 'Structure'],
-          //     ['system_structure/02_consensus.md', 'Consensus'],
-          //     ['system_structure/03_node.md', 'Node'],
-          //     ['system_structure/04_runtime_environment.md', 'Runtime Environment'],
-          //     ['system_structure/05_offchain_worker.md', 'Off-Chain Worker'],
-          //     ['system_structure/06_security.md', 'Security'],
-          //     ['system_structure/07_account_system.md', 'Account System'],
-          //   ],
-          // },
-          // {
-          //   title: 'Cross Chain',
-          //   collapsable: false,
-          //   sidebarDepth: 0,
-          //   children: [
-          //     ['cross_chain/01_cross_chain_mode.md', 'Technology'],
-          //     ['cross_chain/02_interoperate_bridge.md', 'Bridge'],
-          //     ['cross_chain/03_cross_chain_assets.md', 'Assets'],
-          //   ],
-          // },
-          // {
-          //   title: 'Progress',
-          //   collapsable: false,
-          //   sidebarDepth: 0,
-          //   children: [
-          //     ['progress/01_roadmap.md', 'Roadmap'],
-          //     ['progress/02_participants.md', 'Participants'],
-          //   ],
-          // },
-          // '/conclusion.md',
-          // '/DISCLAIMER.md',
-          // '/CHANGELOG.md',
+          {
+            title: 'Getting Started',
+            collapsable: false,
+            sidebarDepth: 0,
+            children: [
+              ['/en/getting_started/01_marketing.md', 'Marketing'],
+              ['/en/getting_started/02_overview.md', 'Overview'],
+            ],
+          },
+          {
+            title: 'Economic',
+            collapsable: false,
+            sidebarDepth: 0,
+            children: [
+              ['/en/economic/01_bnc_economic.md', 'BNC Economic'],
+              ['/en/economic/02_three_pools_valuation_model.md', 'Valuation Model'],
+              ['/en/economic/03_staking_gain_mechanism.md', 'Staking Gain Mechanism'],
+              ['/en/economic/04_stake_proxy_node.md', 'Stake Proxy Node'],
+            ],
+          },
+          {
+            title: 'System Structure',
+            collapsable: false,
+            sidebarDepth: 0,
+            children: [
+              ['/en/system_structure/01_system_structure.md', 'Structure'],
+              ['/en/system_structure/02_consensus.md', 'Consensus'],
+              ['/en/system_structure/03_node.md', 'Node'],
+              ['/en/system_structure/04_runtime_environment.md', 'Runtime Environment'],
+              ['/en/system_structure/05_offchain_worker.md', 'Off-Chain Worker'],
+              ['/en/system_structure/06_security.md', 'Security'],
+              ['/en/system_structure/07_account_system.md', 'Account System'],
+            ],
+          },
+          {
+            title: 'Cross Chain',
+            collapsable: false,
+            sidebarDepth: 0,
+            children: [
+              ['/en/cross_chain/01_cross_chain_mode.md', 'Technology'],
+              ['/en/cross_chain/02_interoperate_bridge.md', 'Bridge'],
+              ['/en/cross_chain/03_cross_chain_assets.md', 'Assets'],
+            ],
+          },
+          {
+            title: 'Progress',
+            collapsable: false,
+            sidebarDepth: 0,
+            children: [
+              ['/en/progress/01_roadmap.md', 'Roadmap'],
+              ['/en/progress/02_participants.md', 'Participants'],
+            ],
+          },
+          {
+            title: 'Conclusion',
+            collapsable: false,
+            path: '/en/conclusion.md',
+            sidebarDepth: 0,
+          },
+          {
+            title: 'Disclaimer',
+            collapsable: false,
+            path: '/en/DISCLAIMER.md',
+            sidebarDepth: 0,
+          },
+          {
+            title: 'Change Log',
+            collapsable: false,
+            path: '/en/CHANGELOG.md',
+            sidebarDepth: 0,
+          }
         ],
       },
       '/zh/': {
@@ -130,8 +145,8 @@ module.exports = {
             collapsable: false,
             sidebarDepth: 0,
             children: [
-              ['zh/getting_started/01_marketing.md', '市场'],
-              ['zh/getting_started/02_overview.md', '概述'],
+              ['/zh/getting_started/01_marketing.md', '市场'],
+              ['/zh/getting_started/02_overview.md', '概述'],
             ],
           },
           {
@@ -139,10 +154,10 @@ module.exports = {
             collapsable: false,
             sidebarDepth: 0,
             children: [
-              ['zh/economic/01_bnc_economic.md', 'BNC 经济'],
-              ['zh/economic/02_three_pools_valuation_model.md', '三池估值模型'],
-              ['zh/economic/03_staking_gain_mechanism.md', 'Staking 增益机制'],
-              ['zh/economic/04_stake_proxy_node.md', 'Stake 代理节点'],
+              ['/zh/economic/01_bnc_economic.md', 'BNC 经济'],
+              ['/zh/economic/02_three_pools_valuation_model.md', '三池估值模型'],
+              ['/zh/economic/03_staking_gain_mechanism.md', 'Staking 增益机制'],
+              ['/zh/economic/04_stake_proxy_node.md', 'Stake 代理节点'],
             ],
           },
           {
@@ -150,13 +165,13 @@ module.exports = {
             collapsable: false,
             sidebarDepth: 0,
             children: [
-              ['zh/system_structure/01_system_structure.md', '架构图'],
-              ['zh/system_structure/02_consensus.md', '共识算法'],
-              ['zh/system_structure/03_node.md', '节点'],
-              ['zh/system_structure/04_runtime_environment.md', '运行时环境'],
-              ['zh/system_structure/05_offchain_worker.md', 'Off-Chain Worker'],
-              ['zh/system_structure/06_security.md', '安全机制'],
-              ['zh/system_structure/07_account_system.md', '账户系统'],
+              ['/zh/system_structure/01_system_structure.md', '架构图'],
+              ['/zh/system_structure/02_consensus.md', '共识算法'],
+              ['/zh/system_structure/03_node.md', '节点'],
+              ['/zh/system_structure/04_runtime_environment.md', '运行时环境'],
+              ['/zh/system_structure/05_offchain_worker.md', 'Off-Chain Worker'],
+              ['/zh/system_structure/06_security.md', '安全机制'],
+              ['/zh/system_structure/07_account_system.md', '账户系统'],
             ],
           },
           {
@@ -164,9 +179,9 @@ module.exports = {
             collapsable: false,
             sidebarDepth: 0,
             children: [
-              ['zh/cross_chain/01_cross_chain_mode.md', '跨链模式'],
-              ['zh/cross_chain/02_interoperate_bridge.md', '互操作转接桥'],
-              ['zh/cross_chain/03_cross_chain_assets.md', '资产跨链'],
+              ['/zh/cross_chain/01_cross_chain_mode.md', '跨链模式'],
+              ['/zh/cross_chain/02_interoperate_bridge.md', '互操作转接桥'],
+              ['/zh/cross_chain/03_cross_chain_assets.md', '资产跨链'],
             ],
           },
           {
@@ -174,26 +189,26 @@ module.exports = {
             collapsable: false,
             sidebarDepth: 0,
             children: [
-              ['zh/progress/01_roadmap.md', '路线图'],
-              ['zh/progress/02_participants.md', '参与方'],
+              ['/zh/progress/01_roadmap.md', '路线图'],
+              ['/zh/progress/02_participants.md', '参与方'],
             ],
           },
           {
             title: '结论',
             collapsable: false,
-            path: 'zh/conclusion.md',
+            path: '/zh/conclusion.md',
             sidebarDepth: 0,
           },
           {
             title: '免责申明',
             collapsable: false,
-            path: 'zh/DISCLAIMER.md',
+            path: '/zh/DISCLAIMER.md',
             sidebarDepth: 0,
           },
           {
             title: '更新日志',
             collapsable: false,
-            path: 'zh/CHANGELOG.md',
+            path: '/zh/CHANGELOG.md',
             sidebarDepth: 0,
           }
         ],
