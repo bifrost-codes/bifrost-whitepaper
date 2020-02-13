7c9f4e62007e9c37e512e8d4182f1ae43bc60865
zzzzy-react <2442545862@qq.com>
2020-02-13 18:45:34 +0800

    测试

---
+++ .vuepress/components/button-demo.vue
@@ -0,0 +1,15 @@
+<template>
+        <g-button></g-button>
+</template>
+<script>
+    import Button from '../src/button'
+    export default {
+        components: {
+            'g-button': Button
+        }
+    }
+    
+</script>
+<style scoped>
+
+</style>
